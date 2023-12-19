6130701932b71ecf227189ec71c1e38368e9d815
Alexandr Tarasiuk <46819896+monstrikwho@users.noreply.github.com>
2023-12-19 11:06:52 +0300

    Utils: Сортировка фото по лайкам и дате добавления

--- src/app/_utils/sortedPhotos.ts
+++ src/app/_utils/sortedPhotos.ts
@@ -1,24 +1,28 @@
 import moment from "moment";
 import { Photo } from "../_services/database";
 
+export type PayloadLikeValue = "unsorted" | "more" | "less";
+
+export type PayloadDateValue = "unsorted" | "recent" | "latest";
+
 export const sortByLikes = (
   photos: Photo[],
-  type: "more" | "less"
+  value: PayloadLikeValue
 ): Photo[] => {
   return photos.sort((a, b) => {
-    if (type === "more") return b.likes - a.likes;
+    if (value === "more") return b.likes - a.likes;
     return a.likes - b.likes;
   });
 };
 
 export const sortByDate = (
   photos: Photo[],
-  type: "recent" | "latest"
+  value: PayloadDateValue
 ): Photo[] => {
   return photos.sort((a, b) => {
     const date_a = moment(a.added_at, "DD.MM.YYYY HH:mm:ss");
     const date_b = moment(b.added_at, "DD.MM.YYYY HH:mm:ss");
-    if (type === "latest") return date_b.diff(date_a);
+    if (value === "latest") return date_b.diff(date_a);
     return date_a.diff(date_b);
   });
 };
