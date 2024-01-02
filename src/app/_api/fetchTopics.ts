a143407f0f9de138832898575e63555ebc41901e
Alexandr Tarasiuk <46819896+monstrikwho@users.noreply.github.com>
2024-01-02 13:52:55 +0300

    FetchTopics: Всего 19 топиков, получаю все, циклом

--- src/app/_api/fetchTopics.ts
+++ src/app/_api/fetchTopics.ts
@@ -1,45 +1,37 @@
-import fakeDelay from "../_utils/delay";
 import { Topic } from "../_providers/Filter";
 
-type FetchTopicsProps = {
-  count: number;
-  delay?: number;
-};
+export const fetchTopics = async () => {
+  let result: Topic[] = [];
 
-export const fetchTopics = async ({ count, delay }: FetchTopicsProps) => {
-  try {
-    let result: Topic[] = [];
-
-    for (let page = 1; result.length < count; page++) {
-      const topics = await fetchMe(page);
-      if (topics) result = result.concat(topics);
-    }
-
-    if (delay) await fakeDelay(delay);
-
-    return result;
-  } catch (error) {
-    console.log("error", error);
-    return [];
+  for (let page = 1; page - 1 < 2; page++) {
+    const topics = await fetchMe(page);
+    if (topics) result = result.concat(topics);
   }
+
+  return result;
 };
 
 async function fetchMe(page: number) {
-  const url = `${process.env.NEXT_PUBLIC_API_URL}/topics?page=${page}&client_id=${process.env.NEXT_PUBLIC_ACCESS_KEY}`;
+  try {
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/topics?page=${page}&client_id=${process.env.NEXT_PUBLIC_ACCESS_KEY}`;
 
-  const res = await fetch(url, { method: "GET" });
+    const res = await fetch(url, { method: "GET" });
 
-  if (res.ok) {
-    const data = await res.json();
-    return data.map((item: any) => ({
-      id: item.id,
-      slug: item.slug,
-      title: item.title,
-      desc: item.description,
-      total_photos: item.total_photos,
-      cover_photo: item.cover_photo.urls.small,
-    }));
+    if (res.ok) {
+      const data = await res.json();
+      return data.map((item: any) => ({
+        id: item.id,
+        slug: item.slug,
+        title: item.title,
+        desc: item.description,
+        total_photos: item.total_photos,
+        cover_photo: item.cover_photo.urls.small,
+      }));
+    }
+
+    return null;
+  } catch (error) {
+    console.log("error", error);
+    return null;
   }
-
-  return null;
 }
